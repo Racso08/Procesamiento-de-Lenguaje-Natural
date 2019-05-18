13dcdff00f9c03ad3a887e40c689f14545e68c92
Racso08 <oscar080899@gmail.com>
2019-05-18 01:53:11 -0600

    Inicio de la gramatica

--- Prolog_Test.pl
+++ Prolog_Test.pl
@@ -1,4 +1,10 @@
-lugar(cartago).
-lugar(sanjose).
-lugar(guanacaste).
-adyacente(cartago,tresrios).
+%Reglas gramaticales
+oracion(O):- sintagma_nominal(SN) , sintagma_verbal(SV), append(SN,SV,O).
+sintagma_nominal(SN):- nombre(SN).
+sintagma_nominal(SN):- articulo(A), nombre(N), append(A,N,SN).
+sintagma_verbal(SV):- verbo(V), sintagma_nominal(SN), append(V,SN,SV).
+
+%Vocabulario
+articulo([el]).
+nombre([gato]).
+verbo([come]).
